76fe688a91dc404e93777222cc1a47b125bb8681
camukund1044 <mukund1044@gmail.com>
2025-10-30 12:29:11 +0530

    Create tally_oldyearopen.py

---
+++ tally_oldyearopen.py
@@ -0,0 +1,36 @@
+
+import pyautogui
+import time
+import pandas as pd
+df=pd.read_excel('E:\\python\\tally_py.xlsx')
+time.sleep(20)
+pyautogui.hotkey('alt', 'tab')
+
+
+time.sleep(5)
+for i in df.itertuples():
+    pyautogui.press("f3")
+    pyautogui.write("Select Company")
+    #pyautogui.press("up")
+    #pyautogui.press("up")
+    pyautogui.press("enter")
+    time.sleep(3)
+    pyautogui.write("Specify Path")
+    #pyautogui.press("up")
+    #pyautogui.press("up")
+    #pyautogui.press("up")
+    pyautogui.press("enter")
+    time.sleep(3)
+    pyautogui.write("Previous:")
+    pyautogui.press("enter")
+    time.sleep(3)
+    #pyautogui.press("enter")
+    pyautogui.write(i.year)
+    pyautogui.press("enter")
+    #pyautogui.press("enter")
+    pyautogui.write("username")
+    pyautogui.press("enter")
+    pyautogui.write("password")
+    #pyautogui.write(i.Date.strftime('%d-%m-%Y'))
+    pyautogui.press("enter")
+    time.sleep(20)
