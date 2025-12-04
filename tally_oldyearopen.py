f4d5e50c991d1a45485d66ba6ba63ce8647a16c8
Mukund Trivedi <mukund1044@gmail.com>
2025-12-04 16:55:10 +0530

    Update tally_oldyearopen.py

--- tally_oldyearopen.py
+++ tally_oldyearopen.py
@@ -2,36 +2,31 @@
 import pyautogui
 import time
 import pandas as pd
-# Change File location as per your requirement
+# 1. Change File location as per your requirement
 df=pd.read_excel('E:\\python\\tally_py.xlsx')
 time.sleep(20)
 pyautogui.hotkey('alt', 'tab')
 
 
 time.sleep(5)
+#2. For looking multiyear file as stored in excel file @ Point no. 1
 for i in df.itertuples():
     pyautogui.press("f3")
+    #3. Select Company is the function in first page after log in to the Tally software
     pyautogui.write("Select Company")
-    #pyautogui.press("up")
-    #pyautogui.press("up")
     pyautogui.press("enter")
     time.sleep(3)
+    #4. Specify Path is the function in second page after selecting Select Company as per point no. 3 in the Tally software
     pyautogui.write("Specify Path")
-    #pyautogui.press("up")
-    #pyautogui.press("up")
-    #pyautogui.press("up")
     pyautogui.press("enter")
     time.sleep(3)
-    pyautogui.write("Previous:")
+    pyautogui.write("As per your File stored in Tally, in my computer it is stored in folder named previous so I write "Previous:"")
     pyautogui.press("enter")
     time.sleep(3)
-    #pyautogui.press("enter")
     pyautogui.write(i.year)
     pyautogui.press("enter")
-    #pyautogui.press("enter")
     pyautogui.write("username")
     pyautogui.press("enter")
     pyautogui.write("password")
-    #pyautogui.write(i.Date.strftime('%d-%m-%Y'))
     pyautogui.press("enter")
     time.sleep(20)
